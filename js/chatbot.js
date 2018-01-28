da99543b455488d0dcd34746155a2be3ba364709
ryantrappy <ryantrapp@live.com>
2018-01-28 01:48:52 -0600

    chatbot now talks to bot

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,3 +1,28 @@
-$( document ).ready(function() {
+    function getNextResponse(text){
+        $(".chatWindow").append(
+            '<div class=\"message to\">' + text + "</div>"
+        );
+        console.log("calling");
+        $.get("http://54.69.108.102:9000/getDialog?dialogText=" + text,
+            function(data,status){
+                console.log("Data: " + data + "\nStatus: " + status);
+                console.log(data);
+                $(".chatWindow").append(
+                    '<div class=\"message from\">' + data.fulfillmentText + "</div>"
+                )
+                // return data;
+            });
+    }
 
-});
+    function getNextResponseAndText(){
+        console.log("submitting");
+        let text = $("#textEntry").val();
+        $("#textEntry").val('')
+        console.log(text);
+        if(text.length > 0){
+            getNextResponse(text);
+        }
+
+    }
+    getNextResponse("Hello");
+    console.log("test")
